5d7754d72baac635a9549c265969bcc17877c3ca
lbi <lbitestme@gmail.com>
2016-11-11 13:10:32 +0200

    end2

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -7,16 +7,18 @@ angular.module('public')
 SignupController.$inject = ['SignUpService'];
 function SignupController(SignUpService) {
   var signupCtrl = this;
-  signupCtrl.invalid_short_name	= true;
+  signupCtrl.totalvalid=false;
   signupCtrl.submit = function () {
     signupCtrl.completed = true;
 	SignUpService.getShortname(signupCtrl.user.short_name).then(function (response) {
 		//console.log(response);
-		if(!response) signupCtrl.invalid_short_name	= true;
+		if(!response) {signupCtrl.invalid_short_name = true;  signupCtrl.totalvalid=false;}
 		else {
+			
 			signupCtrl.invalid_short_name	= false;
 			signupCtrl.user.menu = response;
 			SignUpService.setData(signupCtrl.user);
+			signupCtrl.totalvalid=true;
 			
 		}
 	});
